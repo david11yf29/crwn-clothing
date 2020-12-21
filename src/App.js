9f202734c0d7202e52533d5129effcc863892f60
david11yf29 <david11yf29@gmail.com>
2020-12-21 23:02:07 +0800

    updated

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { Route, Switch, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
+import { createStructuredSelector } from 'reselect'; 
 
 import './App.css';
 
